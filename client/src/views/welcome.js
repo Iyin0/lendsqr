756a83a527e592e8b0ff9bcd14b29c1fecf946cf
Iyin0 <p2iyinoluwa@gmail.com>
2022-10-13 18:58:38 +0100

    Readme.md update

--- client/src/views/welcome.js
+++ client/src/views/welcome.js
@@ -1,17 +1,28 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../scss/welcome.scss'
 
 const Welcome = () => {
 
+    const url = "/api/accounts";
+    const [text, setText] = useState(null)
+
+    fetch(url)
+        .then((res) => res.json())
+        .then((data) => setText(data.message))
+
     return (
         <div className="welcome">
-            <div className="welcome-card">
-                <h1>Welcome to Lendsqr</h1>
-                <div>
-                    <Link to='/login'>Login</Link>
-                    <Link to='/signup'>Signup</Link>
+            {text &&
+                <div className="welcome-card">
+                    <h1>Welcome to Lendsqr</h1>
+                    <h2>{text}</h2>
+                    <div>
+                        <Link to='/login'>Login</Link>
+                        <Link to='/signup'>Signup</Link>
+                    </div>
                 </div>
-            </div>
+            }
         </div>
     );
 }
